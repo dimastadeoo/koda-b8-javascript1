88dc859b323edd0131ca4e19c28e90d520b872e2
dimastadeoo <53636907+dimastadeoo@users.noreply.github.com>
2026-05-13 17:21:06 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let r = 21
-
 let phi
+
+if(typeof r !== "number"){
+    console.log("Eror var R bukan number")
+    return
+}
 
 if (r%7 == 0){
     phi = 22/7
@@ -12,3 +16,4 @@ const K = 2 * phi * r
 
 console.log("Luas Lingkaran "+L)
 console.log("Keliling Lingkaran "+K)
+
