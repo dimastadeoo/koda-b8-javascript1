e4cc093f208ca149c5a2a37d4a148f9ba37a5d54
dimastadeoo <53636907+dimastadeoo@users.noreply.github.com>
2026-05-13 11:45:34 +0700

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,36 @@
+const we = {
+    are: {
+        the: {
+            best: "Koda"
+        }
+    }
+}
+
+console.log(we.are.the.best)
+
+const hello = {
+    world : "Hello World"
+}
+
+console.log(hello.world)
+
+const obj = {
+    str: [
+        undefined,undefined,undefined,[
+            undefined,[
+                undefined,undefined,{
+
+                    man: [
+                            {
+                                tech: {
+                                    academy : "Tech Academy"
+                                }
+                            }
+                        ]
+                }
+            ]
+        ]
+    ]
+}
+
+console.log(obj.str[3][1][2].man[0].tech.academy)
